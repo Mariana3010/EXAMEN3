7116cc2830188c17dab7861fad22926e03519eaa
Mariana3010 <mariana_1320114028@uptecamac.edu.mx>
2022-03-31 10:34:31 -0600

    carga1

---
+++ resources/views/index.blade.php
@@ -0,0 +1,191 @@
+<!DOCTYPE HTML>
+<!--
+	Phantom by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Mariana Sixto</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
+		<link rel="stylesheet" href="/css/main.css" />
+		<noscript><link rel="stylesheet" href="/css/noscript.css" /></noscript>
+	</head>
+	<body class="is-preload">
+		<!-- Wrapper -->
+			<div id="wrapper">
+
+				<!-- Header -->
+					<header id="header">
+						<div class="inner">
+
+							<!-- Logo -->
+								<a>
+									<span class="symbol"><img src="/images/escuela.png" alt="" /></span><span class="title"><h2>Cliente-Servidor</h2></span>
+								</a>
+
+							<!-- Nav -->
+								<!-- <nav>
+									<ul>
+										<li><a href="#menu">Menu</a></li>
+									</ul>
+								</nav> -->
+
+						</div>
+					</header>
+
+			
+
+				<!-- Main -->
+					<div id="main">
+						<div class="inner">
+							<header>
+								<center><h1>Ingeniería en Software<br />
+								<a href="http://uptecamac.edomex.gob.mx/">Universidad Politécnica de Tecámac</a>.<br>
+						    	1522IS<br>
+								<h2>Mariana Sixto Maldonado</h2><br>
+							</h1>
+							    </center>
+								<b><font size="8">Unidad 3</font></b>
+							</header>
+							<section class="tiles">
+								<article class="style1">
+									<span class="image">
+										<img src="/images/Datos.jpg" alt=""> 
+									</span>
+									<a href="/verPublicacion/prueba_1">
+										<h2>¿QUE ES?</h2>
+										<div class="content">							
+										</div>
+									</a>
+								</article>
+								<article class="style2">
+									<span class="image">
+										<img src="/images/col.jpg" alt="" />
+									</span>
+									<a href="/verPublicacion/datos_curiosos">
+										<h2>DATOS CURIOSOS </h2>
+										<div class="content">
+										</div>
+									</a>
+								</article>
+								<article class="style3">
+									<span class="image">
+										<img src="/images/importancia.jpg" alt="" />
+									</span>
+									<a href="/verPublicacion/importancia_colores">
+										<h2>IMPORTANCIA</h2>
+										<div class="content">
+										</div>
+									</a>
+								</article>
+								<article class="style4">
+									<span class="image">
+										<img src="/images/amarillo.jpg" alt="" />
+									</span>
+									<a href="/verPublicacion/color_amarillo">
+										<h2>SIGNIFICADO COLOR AMARILLO</h2>
+										<div class="content">
+										</div>
+									</a>
+								</article>
+								<article class="style5">
+									<span class="image">
+										<img src="/images/naranja.jpg" alt="" />
+									</span>
+									<a href="/verPublicacion/color_naranja">
+										<h2>SIGNIFICADO COLOR NARANJA</h2>
+										<div class="content">
+										</div>
+									</a>
+								</article>
+								<article class="style6">
+									<span class="image">
+										<img src="/images/blanco.jpg" alt="" />
+									</span>
+									<a href="/verPublicacion/color_blanco">
+										<h2>SIGNIFICADO COLOR BLANCO</h2>
+										<div class="content">
+										</div>
+									</a>
+								</article>
+								<article class="style6">
+									<span class="image">
+										<img src="/images/rojo.jpg" alt="" />
+									</span>
+									<a href="/verPublicacion/color_rojo">
+										<h2>SIGNIFICADO COLOR ROJO</h2>
+										<div class="content">
+										</div>
+									</a>
+								</article>
+								<article class="style6">
+									<span class="image">
+										<img src="/images/verde.jpg" alt="" />
+									</span>
+									<a href="/verPublicacion/color_verde">
+										<h2>SIGNIFICADO COLOR VERDE
+										</h2>
+										<div class="content">
+										</div>
+									</a>
+								</article>
+								<article class="style6">
+									<span class="image">
+										<img src="/images/azul.jpg" alt="" />
+									</span>
+									<a href="/verPublicacion/color_azul">
+										<h2>SIGNIFICADO COLOR AZUL
+										</h2>
+										<div class="content">
+										</div>
+									</a>
+								</article>
+							</section>
+							<br>
+								<b><font size="8">API</font></b>
+						
+							<section class="tiles">
+								<article class="style1">
+									<span class="image">
+										<img src="/images/pic01.jpg" alt="" />
+								</span>
+								<a href="/verPublicacion/reconocer_conceptos_presentacion">
+									<h4></h4>
+									<div class="content">
+									</div>
+								</a>
+							</article>
+							</section>
+						</div>
+					</div>
+
+				<!-- Footer -->
+					<footer id="footer">
+						<div class="inner">
+							<section>
+								<h2>Nuestras Redes...</h2>
+								<ul class="icons">
+									<li><a href="#" class="icon brands style2 fa-twitter"><span class="label">Twitter</span></a></li>
+									<li><a href="#" class="icon brands style2 fa-facebook-f"><span class="label">Facebook</span></a></li>
+									<li><a href="#" class="icon brands style2 fa-instagram"><span class="label">Instagram</span></a></li>
+								</ul>
+							</section>
+							<ul class="copyright">
+								<li>&copy; Se trabajo con tecnologia</li><li>Design: <a href="https://openwebinars.net/blog/que-es-html5/">HTML5</a></li>
+							</ul>
+						</div>
+					</footer>
+
+			</div>
+
+		<!-- Scripts -->
+			<script src="/js/jquery.min.js"></script>
+			<script src="/js/browser.min.js"></script>
+			<script src="/js/breakpoints.min.js"></script>
+			<script src="/js/util.js"></script>
+			<script src="/js/main.js"></script>
+
+	</body>
+</html>
